e0f46cef85b033f5fc00bfee01e5747fbfd10d0b
Pierre Buirette <buirette.pierre@gmail.com>
2023-02-03 16:57:13 +0100

    modification pastry entity

--- src/main/java/com/example/demo/repository/entity/Pastry.java
+++ src/main/java/com/example/demo/repository/entity/Pastry.java
@@ -24,28 +24,24 @@ public class Pastry {
     public Long getId() {
         return id;
     }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
-
 
     public Pastry() {
     }
